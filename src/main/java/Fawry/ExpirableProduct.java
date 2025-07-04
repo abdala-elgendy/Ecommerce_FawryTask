6bb1ff2c76f30e592e5e5e2a4ea8dc3f5ed04cef
abdullah <elgendyabdala@gmail.com>
2025-07-04 12:17:12 +0300

    fix: the expirableProduct to be shipped

--- src/main/java/Fawry/ExpirableProduct.java
+++ src/main/java/Fawry/ExpirableProduct.java
@@ -2,16 +2,23 @@ package Fawry;
 
 import java.time.LocalDate;
 
-public class ExpirableProduct extends Product {
+public class ExpirableProduct extends Product implements Shippable {
     private LocalDate expiryDate;
 
-    public ExpirableProduct(String name, double price, int quantity, LocalDate expiryDate) {
+   private double weight;
+    public ExpirableProduct(String name, double price, int quantity, double weight,LocalDate expiryDate) {
         super(name, price, quantity);
         this.expiryDate = expiryDate;
+        this.weight = weight;
     }
 
     @Override
     public boolean isExpired() {
         return LocalDate.now().isAfter(expiryDate);
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
     }
 }
